d685e93e84338342101f81bccbe5173f562d51dc
Josh Eaton <jeaton14@jh.edu>
2022-01-17 20:23:00 -0500

    add style/layout through bootstrap, bones of functionality are there.

--- scripts/library.js
+++ scripts/library.js
@@ -2,15 +2,54 @@ let library = [];
 addBook("Ender's Game", "Orson Scott Card", 324, false);
 addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
 addBook("Red Rising", "Pierce Brown", 382, true);
-let library_container = document.getElementById('library-container');
+// addBook("Ender's Game", "Orson Scott Card", 324, false);
+// addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
+// addBook("Red Rising", "Pierce Brown", 382, true);
+// addBook("Ender's Game", "Orson Scott Card", 324, false);
+// addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
+// addBook("Red Rising", "Pierce Brown", 382, true);
+// addBook("Ender's Game", "Orson Scott Card", 324, false);
+// addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
+// addBook("Red Rising", "Pierce Brown", 382, true);
+// addBook("Ender's Game", "Orson Scott Card", 324, false);
+// addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
+// addBook("Red Rising", "Pierce Brown", 382, true);
+// addBook("Ender's Game", "Orson Scott Card", 324, false);
+// addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
+// addBook("Red Rising", "Pierce Brown", 382, true);
+// addBook("Ender's Game", "Orson Scott Card", 324, false);
+// addBook("Hitchiker's Guide", "Douglas Adams", 832, false);
+// addBook("Red Rising", "Pierce Brown", 382, true);
+
+
+let library_container = document.getElementById('libraryContainer');
 bookDisplayLoop(library_container);
 
 let addBtn = document.getElementById("addBook");
-let form = document.getElementById("newForm");
+let form = document.getElementById("formContainer");
+let cancel = document.getElementById("formRst");
 
 const showForm = function () {
     console.log("pop up the form here");
+    form.setAttribute('class', 'container d-block  justify-content-center mt-3 text-light');
+    library_container.setAttribute('class', 'container d-none');
+    addBtn.disabled = true;
 }
+addBtn.addEventListener('click', showForm);
+
+const hideForm = function () {
+    addBtn.disabled = false;
+    library_container.setAttribute('class', 'row justify-content-center');
+    form.setAttribute('class', 'd-none');
+}
+cancel.addEventListener('click', hideForm);
+
+const submitAdd = function () {
+    console.log("submitted");
+    console.log(form.elements);
+    hideForm();
+}
+form.addEventListener('submit', submitAdd);
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -47,20 +86,38 @@ function addBook(title, author, pages, read) {
 
 function bookDisplayLoop(library_container) {
     library_container.innerHTML = "";
+    let toggle_str = "";
+
+    if (!library.length) {
+        library_container.innerHTML = `
+        <h1>You haven't added any books yet... get reading!</h1>`;
+        return
+    }
     for (let i = 0; i < library.length; i++) {
+        if (library[i].read) {
+            toggle_str = "Read";
+        }
+        else {
+            toggle_str = "Unread";
+        }
         console.log('adding a book');
         let thisBook = document.createElement('div');
-        thisBook.setAttribute('class', 'book-card border border-dark rounded-lg bg-secondary text-light col-sm-6 col-lg-4 p-5 mx-1 my-1');
+        let classStr = 'border border-dark rounded-lg bg-secondary text-light col-md-5 col-lg-3 p-5 mx-1 my-1';
+        let checked = 'checked';
+        if (library[i].read) { classStr = classStr.concat(' read'); }
+        else {
+            checked = 'un'.concat(checked);
+        }
+        thisBook.setAttribute('class', classStr);
         thisBook.innerHTML = `
-        <h1>${library[i].title}</h1>
+        <h2>${library[i].title}</h2>
         <h2>By: ${library[i].author}</h2>
         <h2>pages: ${library[i].pages}</h2>
-        <h2>${library[i].read_string}</h2>
-        <div class="custom-control custom-switch">
-        <input type="checkbox" class="custom-control-input" id="customSwitch${i}">
-        <label class="custom-control-label" for="customSwitch${i}">Toggle this switch element</label>
+        <div class="custom-control custom-switch justify-self-right">
+        <input type="checkbox" class="custom-control-input" ${checked} id="customSwitch${i}">
+        <label class="custom-control-label" for="customSwitch${i}">${toggle_str}</label>
         </div>
-        <button class='btn btn-primary'>Remove</button>
+        <button id='btn${i}' class='btn btn-dark'>Remove</button>
         `
         library_container.appendChild(thisBook);
     }
